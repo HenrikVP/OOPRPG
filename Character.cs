afb1546a7b505756b158483284c493bffedce3c4
Henrik Poulsen <henr140z@elev.tec.dk>
2022-05-03 23:34:59 +0200

    Add project files.

---
+++ Character.cs
@@ -0,0 +1,21 @@
+﻿namespace OOPRPG
+{
+    public enum Role { Fighter = 1, Paladin, Mage, Cleric, Bard, Rouge }
+    public enum Race { Human = 1, HighElf, Hobbit, Dwarf, Orc, Drow }
+
+    public class Character
+    {
+        public string? Name { get; set; }
+        public int Level { get; set; }
+        public int Xp { get; set; }
+        public Role Role { get; set; }
+        public Race Race { get; set; }
+        public Attributes? Attributes { get; set; }
+
+        public void Show()
+        {
+            Console.WriteLine($"\n***Character***\n{Name} the {Role}\tLevel: {Level} ({Xp} xp)\n");
+            if (Attributes != null) Attributes.Show();
+        }
+    }
+}
